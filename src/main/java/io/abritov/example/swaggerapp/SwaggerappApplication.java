ad4e22e3e63a02838fe8efba2c65b6e59d2d11b2
envy124 <pkgsail.dev@gmail.com>
2021-06-10 10:43:29 +0300

    init

---
+++ src/main/java/io/abritov/example/swaggerapp/SwaggerappApplication.java
@@ -0,0 +1,13 @@
+package io.abritov.example.swaggerapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwaggerappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwaggerappApplication.class, args);
+	}
+
+}
